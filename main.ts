4bb0454dec5079d6cdae5a1a53d3b2aa5506485c
410902054 <71751283+410902054@users.noreply.github.com>
2020-10-28 13:06:17 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+for (let i = 0; i <= 4; i++) {
+    for (let j = 0; j <= 4; j++) {
+        if (i % 2 != 0) {
+            led.plot(i, j)
+            basic.pause(100)
+        } else if (false) {
+        	
+        }
+    }
+}
+basic.forever(function () {
+	
+})
